3d438a8152ff04cadc363c91516695bf04ab473c
Martin Kihungi <mwangim4786@gmail.com>
2023-11-03 01:43:45 +0300

    Corrections 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - function to delete an entire binary tree
+ * binary_tree_delete - deletes an entire binary tree
  *@tree: pointer to the root node of the tree to delete.
  */
 void binary_tree_delete(binary_tree_t *tree)
